20312b41d63d3847145e85efb18da55e5c1d847b
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-12-18 13:24:55 -0800

    Removing legacy docs.

--- cleanup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import os
-for filename in ['genindex.html', 'index.html', 'modindex.html', 'search.html']:
-    text = open(filename, 'r').read()
-    f = open(filename, 'w')
-    f.write(text.replace('_static', 'static'))
-    f.close()
